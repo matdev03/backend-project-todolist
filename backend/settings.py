977689fe148d2b88c12de0c01d651ae81353a9e3
matdev03 <webdevmat009@gmail.com>
2025-04-11 03:56:02 +0800

    added requirements

--- backend/settings.py
+++ backend/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "FALSE").lower() == "TRUE"
-
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 
 
