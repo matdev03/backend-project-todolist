631d76d5af0d91962308f549f476b2d4db99f83e
matdev03 <webdevmat009@gmail.com>
2025-04-11 21:12:28 +0800

    Updated Settings database

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,9 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+if database_url:
+    DATABASES["default"] = dj_database_url.parse(database_url)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
